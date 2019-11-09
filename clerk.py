9dc9c5384cbf9a5c8654959d2bc939edfbfd9f5c
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-11-09 21:23:23 +0300

    Vasya - Clerk

---
+++ clerk.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+def tickets(people):
+    cashbox = defaultdict(int)
+    for ticket in people:
+        if ticket == 25:
+            cashbox[25] += 1
+        elif ticket == 50:
+            if cashbox[25] >= 1:
+                cashbox[50] += 1
+                cashbox[25] -= 1
+            else:
+                return 'NO'
+        elif ticket == 100:
+            if cashbox[50] >= 1 and cashbox[25] >= 1:
+                cashbox[100] += 1
+                cashbox[50] -= 1
+                cashbox[25] -= 1
+            elif cashbox[25] >= 3:
+                cashbox[100] += 1
+                cashbox[25] -= 3
+            else:
+                return 'NO'
+    return 'YES'
