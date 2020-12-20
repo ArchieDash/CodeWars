253a390b23054ef04be3b409ab145a8b33e4db9e
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-12-21 02:24:46 +0300

    Return "min max" from string of numbers

--- min_max.py
+++ min_max.py
@@ -1,5 +1,4 @@
-def high_and_low(string):
+"""Return "min max" from string of numbers"""
+def high_and_low(string: str) -> str:
     numbers = [int(n) for n in string.split()]
     return " ".join([str(max(numbers)), str(min(numbers))])
-
-print(high_and_low("1 2 3 4 5"))
